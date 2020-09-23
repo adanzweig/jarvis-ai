3bc198b204369a39ba6be5dd6fd0fe99e246b7fe
adan zweig <adanzweig@gmail.com>
2020-09-23 09:28:57 -0300

    remove keys

--- old/laravel4/vendor/spatie/googlesearch/src/Spatie/GoogleSearch/Interfaces/GoogleSearchInterface.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Spatie\GoogleSearch\Interfaces;
-
-interface GoogleSearchInterface
-{
-    public function getResults($query);
-}
--- old/laravel4/vendor/unicodeveloper/laravel-wikipedia/src/resources/config/wikirand.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-
-    'language' => 'en',
-    'engine'   => 'pedia'
-
-];
