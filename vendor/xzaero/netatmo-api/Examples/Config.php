ddcf1e06966b454b2408b16f8d5f18f675be304c
Adan Zweig <adanzweig+1@gmail.com>
2021-12-10 10:49:31 -0600

    sacar vendor

--- vendor/xzaero/netatmo-api/Examples/Config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$client_id = "5550f7344b5a880860fa8932";
-$client_secret = "pYjr0KLBqjYrYsJ6HUfVofiq6K59UMCmrADg390";
-$test_username = "benjamin.rodriguez@fch.cl";
-$test_password = "Benja034";
-?>
